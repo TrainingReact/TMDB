24ffbfb33a1f550b73c81421bf63fa84ba4f8037
Valentina <valentina_dorazio@virgilio.it>
2021-04-08 15:26:16 +0200

    commit di prova su nuovo branch

--- src/movies/constants/constants.js
+++ src/movies/constants/constants.js
@@ -15,4 +15,6 @@ export const popularUrl = "https://api.themoviedb.org/3/movie/popular?" + api_ke
 export const allFilmUrl = "https://api.themoviedb.org/3/discover/movie?" + api_key + "&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1";
 
 export const searchFilmUrl =
-  "https://api.themoviedb.org/3/discover/movie?api_key=f1789e999f6985421e42f9a8de9c434c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=";
+  "https://api.themoviedb.org/3/discover/movie?" +
+  api_key +
+  "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=";
